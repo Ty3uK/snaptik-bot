cc562a21c3cec9926d44bd1c8baed9ced1eea117
Maksim Karelov <max.karelov@gmail.com>
2024-08-13 21:11:28 +0700

    Rewrite test to table and add telegram-bot-api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Ty3uK/snaptik-bot
 
 go 1.22.5
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
--- internal/resolvers/snap/Decode_test.go
+++ internal/resolvers/snap/Decode_test.go
@@ -5,44 +5,55 @@ import (
 	"testing"
 )
 
-func TestDecodeTikTok(t *testing.T) {
-	input, err := os.ReadFile("../../../test/snap/decode_tiktok_input.txt")
-	if err != nil {
-		t.Fatal(err)
+func TestDecode(t *testing.T) {
+	type Args struct {
+		u int
+		n string
+		t uint64
+		e int
+		r int
+	}
+	type Test struct {
+		name   string
+		input  string
+		output string
+		args   Args
+	}
+	tests := []Test{
+		{
+			name:   "TikTok",
+			input:  "../../../test/snap/decode_tiktok_input.txt",
+			output: "../../../test/snap/decode_tiktok_output.txt",
+			args:   Args{4, "JyivhHWLV", 9, 8, 20},
+		},
+		{
+			name:   "Instagram",
+			input:  "../../../test/snap/decode_insta_input.txt",
+			output: "../../../test/snap/decode_insta_output.txt",
+			args:   Args{20, "RSHywucEs", 49, 3, 20},
+		},
 	}
 
-	output, err := os.ReadFile("../../../test/snap/decode_tiktok_output.txt")
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			input, err := os.ReadFile(tc.input)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	result, err := Decode(string(input), 4, "JyivhHWLV", 9, 8, 20)
-	if err != nil {
-		t.Fatal(err)
-	}
+			output, err := os.ReadFile(tc.output)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if result != string(output) {
-		t.Fatalf("Expected %v, got %v", string(output), result)
-	}
-}
+			result, err := Decode(string(input), tc.args.u, tc.args.n, tc.args.t, tc.args.e, tc.args.r)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-func TestDecodeInstagram(t *testing.T) {
-	input, err := os.ReadFile("../../../test/snap/decode_insta_input.txt")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	output, err := os.ReadFile("../../../test/snap/decode_insta_output.txt")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	result, err := Decode(string(input), 20, "RSHywucEs", 49, 3, 20)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if result != string(output) {
-		t.Fatalf("Expected %v, got %v", string(output), result)
+			if result != string(output) {
+				t.Fatalf("Expected %v, got %v", string(output), result)
+			}
+		})
 	}
 }
