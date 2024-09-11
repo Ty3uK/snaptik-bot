f27672604ddc9fdf5bd28340d4714a150ca2c18e
Maksim Karelov <max.karelov@gmail.com>
2024-09-11 15:33:44 +0700

    Add mp4 parser and fetcher

---
+++ internal/mp4/fetcher.go
@@ -0,0 +1,38 @@
+package mp4
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type FetchResponse struct {
+	Resolution *Resolution
+	Meta       *[]byte
+	Body       io.ReadCloser
+}
+
+func Fetch(httpClient *http.Client, sourceUrl string) (*FetchResponse, error) {
+	res, err := http.Get(sourceUrl)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot make request: %s", err)
+	}
+
+	meta := make([]byte, 512)
+	_, err = res.Body.Read(meta)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot read meta from body: %s", err)
+	}
+
+	resolution, err := ParseResolution(bytes.NewReader(meta))
+	if err != nil || resolution == nil {
+		return nil, fmt.Errorf("Cannot parse resolution: %s", err)
+	}
+
+	return &FetchResponse{
+		Resolution: resolution,
+		Meta:       &meta,
+		Body:       res.Body,
+	}, nil
+}
---
+++ internal/mp4/parser.go
@@ -0,0 +1,73 @@
+package mp4
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+type Box struct {
+	size int64
+	name string
+}
+
+type Resolution struct {
+	Width  uint64
+	Height uint64
+}
+
+func ParseResolution(reader io.ReadSeeker) (*Resolution, error) {
+	var box Box
+	buffer := make([]byte, 8)
+	for {
+		_, err := reader.Read(buffer)
+		if err == io.EOF {
+			return nil, nil
+		}
+		if err != nil {
+			return nil, fmt.Errorf("Cannot read boxSize: %s", err)
+		}
+		box.size = int64(binary.BigEndian.Uint32(buffer[0:4]))
+		box.name = string(buffer[4:8])
+		if box.name == "tkhd" {
+			resolution, err := parseTkhdBox(reader, &buffer, &box)
+			if err != nil {
+				return nil, err
+			}
+			if resolution != nil {
+				return resolution, nil
+			}
+		}
+		if box.name == "moov" || box.name == "trak" {
+			continue
+		}
+		_, err = reader.Seek(box.size-8, io.SeekCurrent)
+		if err != nil {
+			return nil, fmt.Errorf("Cannot seek: %s", err)
+		}
+	}
+}
+
+func parseTkhdBox(reader io.ReadSeeker, buffer *[]byte, box *Box) (*Resolution, error) {
+	_, err := reader.Seek(box.size-16, io.SeekCurrent)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot seek: %s", err)
+	}
+
+	_, err = reader.Read(*buffer)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot read boxSize: %s", err)
+	}
+
+	width := uint64(binary.BigEndian.Uint32((*buffer)[0:4]))
+
+	if width != 0 {
+		height := uint64(binary.BigEndian.Uint32((*buffer)[4:8]))
+		return &Resolution{
+			Height: height / 65536,
+			Width:  width / 65536,
+		}, nil
+	}
+
+	return nil, nil
+}
